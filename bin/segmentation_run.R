24f29f69f088eea3d04bce66f856080fdb4eec5b
nalcala <nalcala@stanford.edu>
2017-01-16 13:58:44 +0100

    update

--- bin/segmentation_run.R
+++ bin/segmentation_run.R
@@ -3,5 +3,5 @@ mode = args[1]
 
 # load results from all chr
 
-finalcall<- segmentbis(Y_qc, Yhat, optK = optK, K = K, sampname_qc,ref_qc, chr, lmax = 200, mode = mode)
+finalcall<- segmentbis(Y_qc, Yhat, optK = globaloptK, K = K, sampname_qc,ref_qc, chr, lmax = 200, mode = mode)
 write.table(finalcall, file = paste(outdir,projectname,'_',cur_chr,'_', optK,'_',mode,'.txt', sep=''), sep='\t', quote=FALSE, row.names=FALSE)
