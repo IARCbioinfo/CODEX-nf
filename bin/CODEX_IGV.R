89bd10a8a2c151d5d86fa3fb6df07fc7df9da580
nalcala <nalcala@stanford.edu>
2017-07-12 09:06:36 +0200

    add scripts to perform conversion to IGV-readable format and annotation

---
+++ bin/CODEX_IGV.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+projectname = args[1]
+optKallchr  = args[2]
+mode        = args[3]
+T_suffix    = args[4]
+
+finalcall.chr=read.table(paste(projectname,'_results_chr1_K', optKallchr,'_',mode,'.txt', sep=''),head=T,sep='\t')
+
+finalcall=finalcall.chr
+
+for(chr in c(2:22,'X','Y')){
+  chr=paste('chr',chr, sep='')
+  cat('chr =',chr,'\n')
+  finalcall.chr=read.table(paste(projectname,'_results_',chr,'_K', optKallchr,'_',mode,'.txt', sep=''),head=T,sep='\t')
+  finalcall=rbind(finalcall,finalcall.chr)
+}
+
+write.table(finalcall,file=paste(paste(projectname,'_results_allchr_K', optKallchr,'_',mode,'.txt', sep=''),sep='\t',quote=F,col.names=T,row.names=F)
+
+tumorsampname=as.matrix(unique(finalcall[,1]))
+
+SEG = c()
+for(i in 1:nrow(tumorsampname)){
+  cat(i,'\t')
+  finalcall.temp=finalcall[which(finalcall$sample_name==tumorsampname[i,1]),]
+  sampname.temp=paste(rep(tumorsampname[i,1],nrow(finalcall.temp)),'_codex',sep='')
+  output=cbind(sampname.temp,finalcall.temp$chr, finalcall.temp$st_bp,finalcall.temp$ed_bp, finalcall.temp$ed_exon-finalcall.temp$st_exon+1, signif(pmax(log(finalcall.temp$copy_no/2,2),-4),4))
+  SEG = rbind(SEG,output)
+}
+
+SEG = as.data.frame(SEG)
+colnames(SEG)=c('Sample', 'Chromosome','Start','End','Num_Probes', 'Segment_Mean')
+
+write.table(SEG,file=paste('all_samples_igv.codex.seg.txt',sep=''),sep='\t',quote=F,col.names=T,row.names=F)
+
+# select T and N
+SEG = SEG[grep(T_suffix,SEG$Sample),]
+
+write.table(SEG,file=paste('T_igv.codex.seg.txt',sep=''),sep='\t',quote=F,col.names=T,row.names=F)
+
+markerfile_1             = SEG[,c(2:3)]
+markerfile_2             = SEG[,c(2,4)]
+colnames(markerfile_1)[2]= "Position"
+colnames(markerfile_2)[2]= "Position"
+markerfile               = rbind(markerfile_1,markerfile_2)
+markerfile               = markerfile[!(duplicated(markerfile)),]
+markerfile$V3            = c(1:nrow(markerfile))
+markerfile$ID            = paste("ID_",markerfile$V3,sep="")
+markerfile               = markerfile[,c(4,1:2)]
+ 
+write.table(markerfile,file = paste('MarkerFile.txt',sep=''),sep='\t',quote=F,col.names=T,row.names=F)
+
+ 
---
+++ bin/annotation.R
@@ -0,0 +1,85 @@
+#!/usr/bin/env Rscript
+
+################################################################
+####          Annotation CNV
+####                Noémie Leblay
+################################################################
+
+args <- commandArgs(trailingOnly=TRUE)
+GTF     = args[1]
+segfile = args[2]
+
+gene2              = read.table(GTF,quote="\"",stringsAsFactors=F,sep="\t",header=F)
+colnames(gene2)[1] = "Chr"
+colnames(gene2)[3] = "info"
+colnames(gene2)[4] = "Start"
+colnames(gene2)[5] = "End"
+
+library (reshape2)
+info2      = colsplit(gene2$V9, ";", c(1:10))
+gene_info2 = cbind(gene2[,-9], info2[,])
+gene_info2 = gene_info2[gene_info2$info=="gene",]
+colnames(gene_info2)[9]  = "gene_id"
+colnames(gene_info2)[10] = "gene_name"
+colnames(gene_info2)[11] = "gene_source"
+colnames(gene_info2)[2]  = "gene_biotype"
+gene_info2               = gene_info2[,1:11]
+
+gene_info2[,"gene_name"]=gsub("^\\s+|\\s+$", "", gene_info2[,10])
+gene_info2$gene_name=unlist(lapply(gene_info2$gene_name,function(x){strsplit(x, " ")[[1]][2]}))
+
+#download the CODEX results files and take a single results file with all the chr
+
+CODEX=read.table(segfile,quote="\"",stringsAsFactors=F,sep="\t",header=T)
+
+#Annotation start and end gene involved in the CNV
+CODEX$Start_gene=unlist(lapply(1:length(CODEX$Start), function(x){res=gene_info2[gene_info2$Start<= CODEX[x, "Start"] &
+                                                                                   gene_info2$End>=CODEX[x, "Start"] &
+                                                                                   gene_info2$Chr==CODEX[x, "Chr"],"gene_name"]
+                                                                  if( length(res)==0){NA} else{paste(res, collapse=",")}}))
+
+
+CODEX$End_gene=unlist(lapply(1:length(CODEX$End), function(x){res=gene_info2[gene_info2$Start<= CODEX[x, "End"] &
+                                                                                 gene_info2$End>=CODEX[x, "End"] &
+                                                                                 gene_info2$Chr==CODEX[x, "Chr"],"gene_name"]
+                                                                if( length(res)==0){NA} else{paste(res, collapse=",")}}))
+
+#Annotation genes involved in the CNV
+
+CODEX$Involved_genes=unlist(lapply(1:length(CODEX$Start), function(x){res=gene_info2[gene_info2$Start>= CODEX[x, "Start"] &
+                                                                                       gene_info2$Start<=CODEX[x, "End"] &
+                                                                                       gene_info2$End<=CODEX[x, "End"] &
+                                                                                       gene_info2$End>=CODEX[x, "Start"] &
+                                                                                       gene_info2$Chr==CODEX[x, "Chr"],"gene_name"]
+                                                                      if( length(res)==0){NA} else{paste(res, collapse=",")}}))
+
+#Annotation exon involved in the CNV for the starting and ending genes
+
+CODEX$Percentage_Start_gene=unlist(lapply(1:length(CODEX$Start), function(x){ if(is.na(CODEX[x,"Start_gene"])) { return(NA)}
+                                                                              print(x)
+                                                                              res_start=gene_info2[gene_info2$Start<= CODEX[x, "Start"] &
+                                                                                                     gene_info2$End>=CODEX[x, "Start"] &
+                                                                                                     gene_info2$Chr==CODEX[x, "Chr"],"Start"]
+                                                                              res_end=gene_info2[gene_info2$Start<= CODEX[x, "Start"] &
+                                                                                                   gene_info2$End>=CODEX[x, "Start"] &
+                                                                                                   gene_info2$Chr==CODEX[x, "Chr"],"End"]
+                                                                              res=unlist(lapply(1: length(res_start), function(i){res_int=intersect(seq(res_start[i], res_end[i], 1), seq (CODEX[x,"Start"], CODEX[x, "End"],1))
+                                                                                                                                  length(res_int)/length(seq(res_start[i], res_end[i], 1))*100
+                                                                              }))
+                                                                              if( length(res)==0){NA} else{paste(res, collapse=",")}}))
+
+CODEX$Percentage_End_gene=unlist(lapply(1:length(CODEX$End), function(x){ if(is.na(CODEX[x,"End_gene"])) { return(NA)}
+                                                                            print(x)
+                                                                            res_start=gene_info2[gene_info2$Start<= CODEX[x, "End"] &
+                                                                                                   gene_info2$End>=CODEX[x, "End"] &
+                                                                                                   gene_info2$Chr==CODEX[x, "Chr"],"Start"]
+                                                                            res_end=gene_info2[gene_info2$Start<= CODEX[x, "End"] &
+                                                                                                 gene_info2$End>=CODEX[x, "End"] &
+                                                                                                 gene_info2$Chr==CODEX[x, "Chr"],"End"]
+                                                                            res=unlist(lapply(1: length(res_start), function(i){res_int=intersect(seq(res_start[i], res_end[i], 1), seq (CODEX[x,"Start"], CODEX[x, "End"],1))
+                                                                                                                                length(res_int)/length(seq(res_start[i], res_end[i], 1))*100
+                                                                            }))
+                                                                            if( length(res)==0){NA} else{paste(res, collapse=",")}}))
+
+write.table(CODEX, file = "allChr_annotated.txt", sep='\t', quote=FALSE, row.names=FALSE)
+
